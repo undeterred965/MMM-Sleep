93c0cd9d557c61db1f15130145b5f1706f619cf0
Jon <62571573+undeterred965@users.noreply.github.com>
2024-10-19 10:21:16 +0100

    Update node_helper.js
    Location of 'bl_power' has changed.
    
    Turning off/on the backlight on the hyperpixel4 should now work.

--- node_helper.js
+++ node_helper.js
@@ -4,9 +4,9 @@ var NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
 	socketNotificationReceived: function(notification, payload) {
 		if (notification === "TURN_DISPLAY_OFF") {
-			exec("echo 1 | sudo tee /sys/class/backlight/rpi_backlight/bl_power",(error,stdout,stderr)=>{});
+			exec("echo 1 | sudo tee /sys/class/backlight/backlight/bl_power",(error,stdout,stderr)=>{});
 		} else if (notification === "TURN_DISPLAY_ON") {
-			exec("echo 0 | sudo tee /sys/class/backlight/rpi_backlight/bl_power",(error,stdout,stderr)=>{});
+			exec("echo 0 | sudo tee /sys/class/backlight/backlight/bl_power",(error,stdout,stderr)=>{});
 		}
 	},
 });
